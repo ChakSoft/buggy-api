e5eb771e1ab30abf2691118d63dc2c37ffc96d00
Michael Chacaton <chako256@gmail.com>
2017-09-07 17:58:12 +0200

    Application structure

---
+++ config/config.dist.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+    /**
+     * Port
+     * 
+     * This value represents the TCP port where the API will wait for incoming HTTP requests.
+     * You can type whatever value you want according to your architecture (between 1024 and 65535).
+     */
+    port : 17811,
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict'
+/**
+ * Buggy API v0.0.1
+ * Licensed under GPL v3. See License file for further information.
+ * 
+ * Created by: Michael Chacaton <chako256@gmail.com>
+ * Released on: 2017-10-31
+ */
+
+const Express = require('express')
+const app = Express()
+
+const Compression = require('compression')
+const BodyParser = require('body-parser')
+const CookieParser = require('cookie-parser')
+
+const Logger = require('./lib/logger')
+const Config = require('./lib/config')
+
+app
+    .use(Compression())
+    .use(BodyParser.json({ extended : true }))
+    .use(CookieParser())
+    
+if (!module.parent) {
+    app.listen(Config.port, () => {
+        Logger.info(`Listening on port ${Config.port}`)
+    })
+}
+
+/**
+ * Exports the app to allow tests with Chai and Mocha
+ */
+module.exports = app
